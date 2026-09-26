8b6bd59a9091402724d46df33c4658ae00abf99b
Aele Sahithi <sahithiaelecloud@gmail.com>
2026-09-27 00:06:53 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sthenofy/',
+
   plugins: [react()],
   resolve: {
     alias: {
